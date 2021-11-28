ac94323c96a8003af7525e006d0806f49fec7837
Advait <advaitpatole@gmail.com>
2021-11-28 03:13:46 -0500

    Updated CMakeLists.txt and added header file

---
+++ include/walker.hpp
@@ -0,0 +1,21 @@
+#include<ros/ros.h>
+#include<sensor_msgs/LaserScan.h>
+#include<geometry_msgs/Twist.h>
+
+class walker{
+	public :
+	walker() ;
+	bool detected ;
+	void LaserCallback(const sensor_msgs::LaserScan::ConstPtr& msg) ;
+	void navigate() ;
+
+	~walker() ;
+
+	private :
+	ros::NodeHandle nh;
+	ros::Subscriber scansub;
+	ros::Publisher velpub;
+
+	geometry_msgs::Twist msg;
+
+}
